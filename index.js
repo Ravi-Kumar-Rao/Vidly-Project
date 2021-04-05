ee6c662380da013a8bf0324c6d31ff9100f48d84
deepikar02 <raoravi.0987@gmail.com>
2021-04-05 13:02:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const customers = require('./routes/customers');
+const genres = require('./routes/genres');
+const express = require('express');
+const app = express();
+
+mongoose.connect('mongodb://localhost/vidly',{ useNewUrlParser: true ,useUnifiedTopology: true })
+.then(()=> console.log('Connectd to mongoose...'))
+.catch(err => console.error('Could not connect to mongoose...'));
+
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
